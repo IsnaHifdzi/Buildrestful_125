a137cf651208529b3c2b714652189730456d9e4d
IsnaHifdzi <hifdziiwh@gmail.com>
2022-12-05 14:10:54 +0700

    first commit

---
+++ src/main/java/pwsaa/pwscintadamaia/PwscintadamaiaApplication.java
@@ -0,0 +1,13 @@
+package pwsaa.pwscintadamaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwscintadamaiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwscintadamaiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pwsaa/pwscintadamaia/PwscintadamaiaApplicationTests.java
@@ -0,0 +1,13 @@
+package pwsaa.pwscintadamaia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwscintadamaiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
